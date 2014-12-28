d96f31f14fdd4c08b6def3b7cfe2827afcae432e
Stix <stix@phcn.de>
2014-12-28 23:05:57 +0100

    SPHPC-4 exchange cuda with opencl libs

--- src/main/java/backend/gpgpu/GPGPUService.java
+++ src/main/java/backend/gpgpu/GPGPUService.java
@@ -1,9 +1,14 @@
 package backend.gpgpu;
 
+import backend.job.Prototype;
+
 public class GPGPUService {
 	
 	public String stateCheck()
 	{
+		Prototype ptt = new Prototype();
+		ptt.run();
+		
 		return "all upp and running";
 	}
 
---
+++ src/main/java/backend/job/Prototype.java
@@ -0,0 +1,9 @@
+package backend.job;
+
+public class Prototype {
+	
+	public void run()
+	{
+	}
+
+}
